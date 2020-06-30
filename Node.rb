6bc20cd80d4be200893c9158de63a447c4dfcc41
Shawn <codingshawn@gmail.com>
2020-06-30 22:55:06 +0800

    Fixed attr_reader syntax error

--- Node.rb
+++ Node.rb
@@ -1,6 +1,6 @@
 class Node
   attr_accessor :left_node, :right_node
-  attr_reader: :data
+  attr_reader :data
   def initialize(data=nil, left_node=nil, right_node=nil)
     @data = data
     @left_node = left_node
